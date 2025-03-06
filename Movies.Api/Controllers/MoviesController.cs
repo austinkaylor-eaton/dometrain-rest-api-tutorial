f9bdaee9470c184eaa61fa5099a4cf94ad1e99b9
Austin Kaylor <austinjkaylor@eaton.com>
2025-03-05 21:09:38 -0500

    Adding an endpoint to create a new movie

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Movies.Application.Repositories;
 using Movies.Contracts.Requests;
 using Movies.Contracts.Responses;
+using Movies.Domain.Models;
 
 namespace Movies.Api.Controllers;
 
@@ -9,5 +10,15 @@ namespace Movies.Api.Controllers;
 [ApiController]
 public class MoviesController(IMovieRepository movieRepository) : ControllerBase
 {
-
+    [HttpPost]
+    public async Task<IActionResult> CreateMovie([FromBody]CreateMovieRequest request)
+    {
+        bool wasMovieCreated = await movieRepository.CreateMovieAsync((Movie)request);
+        return wasMovieCreated switch
+        {
+            true => Created(),
+            false => BadRequest()
+        };
+        //return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, movie);
+    }
 }
