f469ccc9f2ecbac3566f8211c4b04c94f3ed20a5
Austin Kaylor <austinjkaylor@eaton.com>
2025-03-05 21:34:22 -0500

    Adding HTTP request example for creating a movie and updating launch settings for HTTPS

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -10,15 +10,42 @@ namespace Movies.Api.Controllers;
 [ApiController]
 public class MoviesController(IMovieRepository movieRepository) : ControllerBase
 {
+    /// <summary>
+    /// Get a <see cref="Movie"/> by its unique identifier
+    /// </summary>
+    /// <param name="id">The <see cref="Movie"/>s unique identifier</param>
+    /// <returns>
+    /// <para>An <see cref="OkResult"/> containing the <see cref="Movie"/></para>
+    /// <para>A <see cref="NotFoundResult"/> if the <see cref="Movie"/> was not found</para>
+    /// </returns>
+    [HttpGet("{id:Guid}")]
+    public async Task<IActionResult> GetMovie(Guid id)
+    {
+        Movie? movie = await movieRepository.GetMovieByIdAsync(id);
+
+        return movie is not null
+            ? Ok((MovieResponse)movie)
+            : NotFound();
+    }
+
+    /// <summary>
+    /// Create a new <see cref="Movie"/>
+    /// </summary>
+    /// <param name="request">The <see cref="CreateMovieRequest"/>, representing the new <see cref="Movie"/> to create</param>
+    /// <returns>
+    /// <para>A <see cref="CreatedAtActionResult"/> containing the newly created <see cref="Movie"/> with a location to get the new <see cref="Movie"/></para>
+    /// <para>A <see cref="BadRequestResult"/> if the <see cref="Movie"/> could not be created</para>
+    /// </returns>
     [HttpPost]
     public async Task<IActionResult> CreateMovie([FromBody]CreateMovieRequest request)
     {
-        bool wasMovieCreated = await movieRepository.CreateMovieAsync((Movie)request);
+        Movie movieToCreate = (Movie)request;
+        bool wasMovieCreated = await movieRepository.CreateMovieAsync(movieToCreate);
+
         return wasMovieCreated switch
         {
-            true => Created(),
+            true => CreatedAtAction(nameof(GetMovie), new { id = movieToCreate.Id }, movieToCreate),
             false => BadRequest()
         };
-        //return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, movie);
     }
 }
