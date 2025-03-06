a2d2a1d6b33f264694876773e86901cce43f8d7a
Austin Kaylor <austinjkaylor@eaton.com>
2025-03-05 22:22:49 -0500

    Adding an explicit conversion operator to convert a List<Movies> to a MovieResponse

--- Movies.Contracts/Responses/MoviesResponse.cs
+++ Movies.Contracts/Responses/MoviesResponse.cs
@@ -1,4 +1,6 @@
-﻿namespace Movies.Contracts.Responses;
+﻿using Movies.Domain.Models;
+
+namespace Movies.Contracts.Responses;
 
 /// <summary>
 /// A response containing a collection of Movies.
@@ -9,4 +11,15 @@ public class MoviesResponse
     /// A collection of Movies.
     /// </summary>
     public required IEnumerable<MovieResponse> Movies { get; init; } = [];
+
+    /// <summary>
+    /// An explicit conversion operator that converts a collection of <see cref="Movie"/>s to a <see cref="MoviesResponse"/>.
+    /// </summary>
+    public static explicit operator MoviesResponse(List<Movie> movies)
+    {
+        return new MoviesResponse
+        {
+            Movies = movies.Select(movie => (MovieResponse)movie)
+        };
+    }
 }
